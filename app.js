e79bc8e264a029ebf4a2992e5441cc5719ef7b38
Kaijam Jeffers <faceless128@gmail.com>
2022-03-31 00:38:08 -0400

    cleaned up code

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = (profileDataArr) => {
     for (let i = 0; i < profileDataArr.length; i++) {
         console.log(profileDataArr[i])
     }
     console.log('================');
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-    });
-}
+    profileDataArr.forEach((profileItem) => console.log(profileItem));
+};
 
 printProfileData(profileDataArgs);
