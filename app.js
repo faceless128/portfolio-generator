24d7bc269f41811430b4595a76d5c779e98dc35c
Kaijam Jeffers <faceless128@gmail.com>
2022-04-04 16:23:53 -0400

    added style

--- app.js
+++ app.js
@@ -134,8 +134,19 @@ promptUser()
     .then(portfolioData => {
         const pageHTML = generatePage(portfolioData);
 
-        fs.writeFile('./index.html', pageHTML, err => {
-            if (err) throw err;
-        console.log('Portfolio complete! Check out index.html to see the output!')
+        fs.writeFile('./dist/index.html', pageHTML, err => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log('Page created! CHeck out the index.html in this directory to see it!');
+            fs.copyFile('./src/style.css','./dist/syle.css', err => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }    
+                console.log('Style sheet copied succesfully!')
+            })
+    
+        });
     });
-});
