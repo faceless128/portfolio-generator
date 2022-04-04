7609f3f55af9efd1bb9954a4e578f731cd054a07
Kaijam Jeffers <faceless128@gmail.com>
2022-04-04 15:41:36 -0400

    generating HTML from questions

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -129,14 +131,11 @@ Add a New Project
 
 promptUser()
     .then(promptProject)
-    .then(portfolioData => console.log(portfolioData));
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+    .then(portfolioData => {
+        const pageHTML = generatePage(portfolioData);
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!')
-// });
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw err;
+        console.log('Portfolio complete! Check out index.html to see the output!')
+    });
+});
